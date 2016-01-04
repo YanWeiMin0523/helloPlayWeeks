b92e49fde1682452646fe61b03497ddfda11a740
YanWeiMin0523 <1379556026@qq.com>
2016-01-04 11:23:25 +0800

    first

---
+++ helloPlayWeeks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloPlayWeeks
+//
+//  Created by scjy on 16/1/4.
+//  Copyright © 2016年 YanWeiMin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
