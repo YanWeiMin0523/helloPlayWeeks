f5bf8adb7fbe54decf5815cf9f0991512b7ff342
YanWeiMin0523 <1379556026@qq.com>
2016-01-04 17:11:31 +0800

    App

--- helloPlayWeeks/AppDelegate.m
+++ helloPlayWeeks/AppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import "MainViewController.h"
+#import "MineViewController.h"
+#import "DiscoverViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -18,7 +21,45 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     
+    //UITabBarCOntroller
+    UITabBarController *tabBarVC = [[UITabBarController alloc] init];
+    //创建被tabBar管理的视图控制器
+    //主页
+    UIStoryboard *mainStory = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    UINavigationController *mainNav = mainStory.instantiateInitialViewController;
+    mainNav.tabBarItem.image = [UIImage imageNamed:@"ft_home_normal_ic"];
+    UIImage *mainImage = [UIImage imageNamed:@"ft_home_selected_ic"];
+    //选中的图片,按照原始图片状态显示
+    mainNav.tabBarItem.selectedImage = [mainImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    //调整tabBar图片显示位置：按照上，左，下，右设置
+    mainNav.tabBarItem.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
     
+    //发现
+    UIStoryboard *discoverStory= [UIStoryboard storyboardWithName:@"Discover" bundle:nil];
+    UINavigationController *discoverNav = discoverStory.instantiateInitialViewController;
+    discoverNav.tabBarItem.image = [UIImage imageNamed:@"ft_found_normal_ic"];
+    UIImage *discoverImage = [UIImage imageNamed:@"ft_found_selected_ic"];
+    //选中的图片
+    discoverNav.tabBarItem.selectedImage = [discoverImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    discoverNav.tabBarItem.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+    
+    //我的
+    UIStoryboard *mineStory= [UIStoryboard storyboardWithName:@"Mine" bundle:nil];
+    UINavigationController *mineNav = mineStory.instantiateInitialViewController;
+    mineNav.tabBarItem.image = [UIImage imageNamed:@"ft_person_normal_ic"];
+    UIImage *mineImage = [UIImage imageNamed:@"ft_person_selected_ic"];
+    //选中的图片
+    mineNav.tabBarItem.selectedImage = [mineImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    //设置图片位置
+    mineNav.tabBarItem.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+    
+    //添加被管理的视图控制器
+    tabBarVC.viewControllers = @[mainNav, discoverNav, mineNav];
+    //设置tabBar导航栏的颜色
+    tabBarVC.tabBar.barTintColor = [UIColor whiteColor];  
+    
+    
+    self.window.rootViewController = tabBarVC;
     
     
     
